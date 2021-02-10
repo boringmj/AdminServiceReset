cee51618e972851731c8c03da9b8eb9360085fe0
boringmj <2878663643@qq.com>
2021-02-11 04:25:37 +0800

    修复数据库连接失败无法正常检测问题

--- MoJi/module/Database.module.php
+++ MoJi/module/Database.module.php
@@ -4,6 +4,14 @@
 if(DATABASE_ENABLE&&class_exists('Database'))
 {
     debug_log(LANGUAGE_LOG_DATABASE_NAME,LANGUAGE_LOG_DATABASE_SUCCESS,__FILE__);
+    if(CONFIG_DATABASE_HOST==''||CONFIG_DATABASE_USER==''||CONFIG_DATABASE_PASSWORD==''||CONFIG_DATABASE_DATABASE=='')
+    {
+        write_log(LANGUAGE_LOG_DATABASE_ERROR_TITLE,'Configuration options cannot be empty',__FILE__,20);
+        if(APPLICATION_DEBUG)
+            exit(LANGUAGE_DATABASE_ERROR_DEBUG);
+        else
+            exit(LANGUAGE_DATABASE_ERROR);
+    }
     $Database=new Database();
     $Database->SetHost(CONFIG_DATABASE_HOST);
     $Database->SetUser(CONFIG_DATABASE_USER);
