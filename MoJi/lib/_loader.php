d4992039cc43e8da841ddb23b983f12e3662e1d2
boringmj <2878663643@qq.com>
2022-03-18 05:17:55 +0800

    取消AES支持

--- MoJi/lib/_loader.php
+++ MoJi/lib/_loader.php
@@ -5,7 +5,6 @@ require __DIR__.'/Ipquery/ipquery.class.php';
 
 //第三方库
 require __DIR__.'/PHPMailer/phpmailer.class.php';
-require __DIR__.'/AES/AES.php';
 require __DIR__.'/JavaScriptPacker/jspacker.php';
 
 ?>
